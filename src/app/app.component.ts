f3f8860f8d628078d815b2c29dd33ef1619dd788
japps1a22 <japps1a22@cicct.local>
2017-08-29 11:10:37 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input,EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+@Output () sendOutput= new EventEmitter <any>();
+@Input () studentList = [];
+dataCollection = [];
+printFlag = false;
+printProceed;
+
+getData(event){
+this.dataCollection = event;
+console.log(event);
 }
+
+printData(event){
+this.printFlag = event;
+console.log(event);
+if(this.printFlag){
+  if(this.dataCollection.length == 0)
+	this.printProceed = false;
+else
+	this.printProceed = true;
+}
+}
+}
+
